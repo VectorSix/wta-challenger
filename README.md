b294e5814e263cdc3d7f2d06f6d1fa4b5a4d7b62
Severin Holm <info@severin-holm.ch>
2021-08-16 20:10:57 +0200

    Bugfix
    - Last Changes

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-
 ![alt text](https://cloud.severin-holm.ch/cloudify/assets/img/logo/severin-holm.png "severin-holm.ch")
 
-# NodeJs_Boilerplate
-Node JS Boilerplate Application
+# WebTech Advanced - Code Challenger
+
+Das HF-ICT WebTech Advanced Challenger Projekt dient als gemeinsame Ausgangslage für diverse JavaScript Code Challenges. Diese Applikation wird im Verlauf des Moduls immer weiter ausgebaut und mit neuen Challenges angereichert.
 
 # Install
+
 ```
 npm install
 ```
 
 # Run
+
 ```
 npm run dev
 npm run start
--- app/business/LessonArray.js
+++ app/business/LessonArray.js
@@ -2,65 +2,65 @@
  * Lesson One
  * @param {*} opt
  */
- const LessonArray = function (opt) {
-    let $this = this;
-    let _private = {};
-    let server = null;
- 
-    // Options
-    let options = Object.assign(
-       {},
-       typeof opt !== "undefined" ? opt : {}
-    );
+const LessonArray = function (opt) {
+   let $this = this;
+   let _private = {};
+   let server = null;
 
-    /**
-     * GetCapitalLetters
-     * [Simple]
-     * 
-     * @param {Char Array} arrayData ['a', 'b', 'C', 'D',...]
-     * @returns array ['C', 'D']
-     */
-    this.getCapitalLetters = (arrayData) => {
-        let result = [];
-        return result;
-    }
+   // Options
+   let options = Object.assign({}, typeof opt !== "undefined" ? opt : {});
 
-    /**
-     * ArrayUnification
-     * [Simple]
-     * 
-     * @param {String Array} arrData ['red', 'green', 'red', 'blue',...]
-     * @returns array ['red', 'green', 'blue',...]
-     */
-    this.arrayUnification = (arrData) => {
-        let result = [];
-        return result;
-    }
+   /**
+    * GetCapitalLetters
+    * [Simple]
+    * Diese Methode nimmt einen Array als Parameter und gibt nur die Array Nodes welche mit Grossbuchstaben gefüllt sind, zurück.
+    * Wenn Wörter oder Zahlen im Array stehen, werden diese nicht berücksichtigt (werden nicht ausgegeben).
+    * @param {Char Array} arrayData ['a', 'b', 'C', 'D']
+    * @returns array ['C', 'D']
+    */
+   this.getCapitalLetters = (arrayData) => {
+      let result = [];
+      return result;
+   };
 
-    /**
-     * SortArray
-     * [Medium]
-     * 
-     * @param {*} arrData 
-     * @returns 
-     */
-    this.sortArray = (arrData) => {
-        let result = [];
-        return result;        
-    }
+   /**
+    * ArrayUnification
+    * [Simple]
+    * Diese Methode eliminiert alle doppelt vorkommenden Werte in einem Array. Der Rückgabe wert ist ein Array, bei dem jedes
+    * Element nur einmalig vorkommt.
+    * @param {String Array} arrData ['red', 'green', 'red', 'blue']
+    * @returns array ['red', 'green', 'blue']
+    */
+   this.arrayUnification = (arrData) => {
+      let result = [];
+      return result;
+   };
 
-    /**
-     * Find In Array (Recursion)
-     * [Hard]
-     * 
-     * @param {String Array} arrData [multi dim array]
-     * @param {String} needle Search String
-     * @returns 
-     */
-     this.findInArray = (arrData, needle) => {
-        let result = [];
-        return result;
-    }
- }
+   /**
+    * SortArray
+    * [Medium]
+    * Diese Methode sortiert einen Array nach alphabetischer Reihenfolge und gibt den sortierten Array als Rückgabewert zurück.
+    * @param {String Array} arrData ["Auto", "Velo", "E-Bike", "Skateboard"]
+    * @returns array ["Auto", "E-Bike", "Skateboard", "Velo"]
+    */
+   this.sortArray = (arrData) => {
+      let result = [];
+      return result;
+   };
 
- module.exports = LessonArray;
+   /**
+    * Find In Array
+    * [Hard]
+    * Diese Methode akzeptiert einen mehrdimensionalen Array als Parameter und einen Suchbegriff (needle). Wenn der Suchbegriff
+    * innerhalb des Arrays vorkommt, wird ein TRUE und sonst ein FALSE ausgegeben. Profis verwenden bitte den rekursiven Ansatz.
+    * @param {String Array} arrData ["Auto", ["Banane", "Apfel", ["grün", "rot", "gelb"]]]
+    * @param {String} needle rot
+    * @returns Boolean TRUE
+    */
+   this.findInArray = (arrData, needle) => {
+      let result = false;
+      return result;
+   };
+};
+
+module.exports = LessonArray;
